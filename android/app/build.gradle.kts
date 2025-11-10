41d8f5649b7b33152a636ef4fdaae4bb81000db6
Ramazan <Ramazan@RAMBO3>
2025-11-10 16:04:38 +0300

    43

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,14 +20,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.rmos.kabultalepsayim"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        targetSdk = 36
+        versionCode = 9
+        versionName = "1.0.9"
     }
 
     buildTypes {
